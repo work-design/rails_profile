f976e510254878a8caaae3fc845092745f6e768a
qinmingyuan <mingyuan0715@foxmail.com>
2019-03-29 09:36:18 +0800

    age

--- app/models/rails_profile/profile.rb
+++ app/models/rails_profile/profile.rb
@@ -33,6 +33,12 @@ class Profile < ApplicationRecord
     r_hash[:year]
   end
 
+  def age_str
+    return "未知" unless self.birthday
+    r_hash = TimeHelper.exact_distance_time(self.birthday, Date.today)
+    "#{r_hash[:year]}岁#{r_hash[:month]}月"
+  end
+
   def avatar_url
     avatar.service_url if avatar.attachment.present?
   end
