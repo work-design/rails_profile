12c82b47d5acca3c359d63a58719f7cd3d625c3f
qinmingyuan <mingyuan0715@foxmail.com>
2019-06-13 19:47:50 +0800

    sync user id

--- app/models/rails_profile/account.rb
+++ app/models/rails_profile/account.rb
@@ -2,7 +2,7 @@ module RailsProfile::Account
   extend ActiveSupport::Concern
   included do
     has_many :profiles, foreign_key: :identity, primary_key: :identity
-    after_save :sync_to_profiles, if: -> { saved_change_to_identity? || saved_change_to_confirmed? }
+    after_save :sync_to_profiles, if: -> { saved_change_to_identity? || saved_change_to_user_id? || saved_change_to_confirmed? }
   end
 
   def sync_to_profiles
