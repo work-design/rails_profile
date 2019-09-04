20ea67c33bc942a7c521c41c402b4e186204ad0a
qinmingyuan <mingyuan0715@foxmail.com>
2019-09-04 12:05:48 +0800

    fix as

--- app/models/rails_profile/agent.rb
+++ app/models/rails_profile/agent.rb
@@ -2,7 +2,7 @@ module RailsProfile::Agent
   extend ActiveSupport::Concern
 
   included do
-    has_many :proteges, class_name: 'Tutelage', foreign_key: :tutelar_id, dependent: :delete_all, inverse_of: :tutelar
+    has_many :proteges, class_name: 'Agency', foreign_key: :agent_id, dependent: :delete_all, inverse_of: :agent
     has_many :clients, through: :proteges
     has_many :agent_maintains, class_name: 'Maintain', foreign_key: :agent_id, inverse_of: :tutelar
     accepts_nested_attributes_for :proteges, reject_if: :all_blank
