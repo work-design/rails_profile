f632b3eea317814b385524a0d618e5414fa1c66c
qinmingyuan <mingyuan0715@foxmail.com>
2019-12-03 16:17:43 +0800

    fix as name

--- app/models/rails_profile/client.rb
+++ app/models/rails_profile/client.rb
@@ -4,7 +4,7 @@ module RailsProfile::Client
   included do
     has_many :client_agencies, class_name: 'Agency', as: :client, dependent: :delete_all, inverse_of: :client
     Agency.agent_types.each do |agent_type|
-      has_many :"#{agent_type.underscore}_agents", through: :agencies, source: :agent, source_type: agent_type
+      has_many :"#{agent_type.underscore}_agents", through: :client_agencies, source: :agent, source_type: agent_type
     end
     has_many :client_maintains, class_name: 'Maintain', foreign_key: :client_id, inverse_of: :client
     accepts_nested_attributes_for :client_agencies, reject_if: :all_blank
