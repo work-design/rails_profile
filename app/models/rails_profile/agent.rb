871279b02ad351f2fb8c670db64a6c2c10fb610a
qinmingyuan <mingyuan0715@foxmail.com>
2019-12-03 09:44:01 +0800

    update agent type

--- app/models/rails_profile/agent.rb
+++ app/models/rails_profile/agent.rb
@@ -2,10 +2,10 @@ module RailsProfile::Agent
   extend ActiveSupport::Concern
 
   included do
-    has_many :proteges, class_name: 'Agency', foreign_key: :agent_id, dependent: :delete_all, inverse_of: :agent
-    has_many :clients, through: :proteges
+    has_many :agent_agencies, class_name: 'Agency', foreign_key: :agent_id, dependent: :delete_all, inverse_of: :agent
+    has_many :clients, through: :agencies
     has_many :agent_maintains, class_name: 'Maintain', foreign_key: :agent_id, inverse_of: :agent
-    accepts_nested_attributes_for :proteges, reject_if: :all_blank
+    accepts_nested_attributes_for :agent_agencies, reject_if: :all_blank
   end
 
 
--- app/models/rails_profile/client.rb
+++ app/models/rails_profile/client.rb
@@ -2,10 +2,10 @@ module RailsProfile::Client
   extend ActiveSupport::Concern
 
   included do
-    has_many :agencies, foreign_key: :client_id, dependent: :delete_all, inverse_of: :client
+    has_many :client_agencies, class_name: 'Agency', foreign_key: :client_id, dependent: :delete_all, inverse_of: :client
     has_many :agents, through: :agencies
     has_many :client_maintains, class_name: 'Maintain', foreign_key: :client_id, inverse_of: :client
-    accepts_nested_attributes_for :agencies, reject_if: :all_blank
+    accepts_nested_attributes_for :client_agencies, reject_if: :all_blank
   end
 
 
