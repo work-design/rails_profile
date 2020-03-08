eb23b99ed012b21a2f37acffd0b0bfae0f26d06e
qinmingyuan <mingyuan0715@foxmail.com>
2020-03-08 12:12:31 +0800

    wait address

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,6 @@
 class Address < ApplicationRecord
   include RailsProfile::Address
   include RailsFactory::Address if defined? RailsFactory
+  include RailsShip::Address if defined? RailsShip
+  include RailsWait::Address if defined? RailsWait
 end unless defined? Address
