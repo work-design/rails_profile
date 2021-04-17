2c2131981f1dd03d41cb7ffb7b461f8437746efa
qinmingyuan <mingyuan0715@foxmail.com>
2021-04-17 21:40:56 +0800

    moved search code

--- app/controllers/profiled/areas_controller.rb
+++ app/controllers/profiled/areas_controller.rb
@@ -4,20 +4,7 @@ module Profiled
     def index
     end
 
-    def search
-      if params[:nation]
-        @areas = Area.where(nation: params[:nation]).select(:province).distinct
-        results = @areas.map { |x| { value: x.province, text: x.province, name: x.province } }
-      elsif params[:province]
-        @areas = Area.where(province: params[:province]).select(:city, :id).distinct
-        results = @areas.map { |x| { value: x.id, text: x.city, name: x.city } }
-      else
-        results = []
-      end
-
-      render json: { values: results }
-    end
-
+    # for weui.js
     def list
       values = Profiled::Area.list(value_name: 'value', label_name: 'label')
       render json: { values: values }
