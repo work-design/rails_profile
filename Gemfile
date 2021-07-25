99831ffa3262a784881ed2da38ab7bc9b18fe37d
qinmingyuan <mingyuan0715@foxmail.com>
2021-07-25 13:39:56 +0800

    gemfile

--- Gemfile
+++ Gemfile
@@ -2,7 +2,9 @@ source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 gemspec
 
+gem 'pg'
 gem 'jbuilder'
 
 gem 'rails_com', github: 'work-design/rails_com'
 gem 'rails_auth', github: 'work-design/rails_auth'
+gem 'viter', github: 'qinmingyuan/viter'
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,19 +1,13 @@
-# Configure Rails Environment
-ENV["RAILS_ENV"] = "test"
+ENV['RAILS_ENV'] = 'test'
+require_relative 'dummy/config/environment'
+require 'rails/test_help'
+require 'minitest/mock'
 
-require_relative "../test/dummy/config/environment"
-ActiveRecord::Migrator.migrations_paths = [File.expand_path("../test/dummy/db/migrate", __dir__)]
-require "rails/test_help"
-
-# Filter out Minitest backtrace while allowing backtrace from other libraries
-# to be shown.
 Minitest.backtrace_filter = Minitest::BacktraceFilter.new
 
-
-# Load fixtures from the engine
-if ActiveSupport::TestCase.respond_to?(:fixture_path=)
-  ActiveSupport::TestCase.fixture_path = File.expand_path("fixtures", __dir__)
-  ActionDispatch::IntegrationTest.fixture_path = ActiveSupport::TestCase.fixture_path
-  ActiveSupport::TestCase.file_fixture_path = ActiveSupport::TestCase.fixture_path + "/files"
-  ActiveSupport::TestCase.fixtures :all
+class ActiveSupport::TestCase
+  self.fixture_path = File.expand_path('fixtures', __dir__)
+  self.file_fixture_path = fixture_path + '/files'
+  ActionDispatch::IntegrationTest.fixture_path = self.fixture_path
+  fixtures :all
 end
