00785b762845f5d6aea4ecb4a16b0fd532c737a5
qinmingyuan <mingyuan0715@foxmail.com>
2021-10-19 21:12:43 +0800

    profiled

--- app/controllers/profiled/admin/address_organs_controller.rb
+++ app/controllers/profiled/admin/address_organs_controller.rb
@@ -1,20 +1,20 @@
 module Profiled
   class Admin::AddressOrgansController < Admin::BaseController
-    before_action :set_new_address_user, only: [:new, :create]
+    before_action :set_new_address_organ, only: [:new, :create]
 
     def index
       @address_organs = current_organ.address_organs.includes(address: :area).page(params[:page])
     end
 
     private
-    def set_new_address_user
-      @address_user = @address.address_users.build(**params.permit(:user_id))
+    def set_new_address_organ
+      @address_organ = current_organ.address_organs.build
     end
 
-    def address_user_params
-      params.fetch(:address_user, {}).permit(
-        :user_id,
-        :state
+    def address_organ_params
+      params.fetch(:address_organ, {}).permit(
+        :state,
+        :default
       )
     end
 
