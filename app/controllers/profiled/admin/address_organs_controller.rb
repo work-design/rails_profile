acb8c73ae7f682272a724d307bb62cd1f858f6f5
qinmingyuan <mingyuan0715@foxmail.com>
2021-11-09 21:08:19 +0800

    addaddress in admin

--- app/controllers/profiled/admin/address_organs_controller.rb
+++ app/controllers/profiled/admin/address_organs_controller.rb
@@ -1,6 +1,7 @@
 module Profiled
   class Admin::AddressOrgansController < Admin::BaseController
-    before_action :set_new_address_organ, only: [:new, :create]
+    before_action :set_new_address_organ, only: [:new]
+    before_action :set_create_address_organ, only: [:create]
 
     def index
       @address_organs = current_organ.address_organs.includes(address: :area).page(params[:page])
@@ -9,12 +10,19 @@ module Profiled
     private
     def set_new_address_organ
       @address_organ = current_organ.address_organs.build
+      @address_organ.build_address
+      @address_organ.address.area = Area.new
+    end
+
+    def set_create_address_organ
+      @address_organ = current_organ.address_organs.build(address_organ_params)
     end
 
     def address_organ_params
       params.fetch(:address_organ, {}).permit(
         :state,
-        :default
+        :default,
+        address_attributes: {}
       )
     end
 
--- app/models/profiled/model/address_organ.rb
+++ app/models/profiled/model/address_organ.rb
@@ -9,6 +9,7 @@ module Profiled
       belongs_to :organ, class_name: 'Org::Organ', inverse_of: :address_organs
 
       belongs_to :address, inverse_of: :address_organs
+      accepts_nested_attributes_for :address
 
       has_many :shipments, as: :shipping
 
@@ -17,6 +18,12 @@ module Profiled
         forwarder: 'forwarder',
         invoice: 'invoice'
       }
+
+      after_update :set_default, if: -> { default? && saved_change_to_default? }
+    end
+
+    def set_default
+      self.class.where.not(id: self.id).where(organ_id: self.organ_id).update_all(default: false)
     end
 
   end
