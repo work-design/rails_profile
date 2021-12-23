58d6f4df824d7f266eadec99b57673838db2c4a4
qinmingyuan <mingyuan0715@foxmail.com>
2021-12-23 09:50:45 +0800

    ext address

--- app/models/profiled/address.rb
+++ app/models/profiled/address.rb
@@ -2,7 +2,7 @@ module Profiled
   class Address < ApplicationRecord
     include Model::Address
     include Factory::Model::Address if defined? RailsFactory
-    include Ship::Model::Address if defined? RailsShip
+    include Ship::Ext::Address if defined? RailsShip
     include Wait::Model::Address if defined? RailsWait
   end
 end
