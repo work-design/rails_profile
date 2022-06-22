fb99bb561520c2a867daf0c9a634d10454d06678
qinmingyuan <mingyuan0715@foxmail.com>
2022-06-22 11:00:43 +0800

    add resss use

--- app/models/profiled/model/address_use.rb
+++ app/models/profiled/model/address_use.rb
@@ -3,19 +3,13 @@ module Profiled
     extend ActiveSupport::Concern
 
     included do
-      attribute :commission_ratio, :decimal, precision: 4, scale: 2, default: 0, comment: '佣金比例'
-
-      belongs_to :address, inverse_of: :address_users
-      belongs_to :user, inverse_of: :address_users, class_name: 'Auth::User'
-      belongs_to :inviter, class_name: 'Auth::User', optional: true
-
-      has_many :shipments, as: :shipping
-
       enum kind: {
         transport: 'transport',
         forwarder: 'forwarder',
         invoice: 'invoice'
       }
+
+      belongs_to :address, inverse_of: :address_uses
     end
 
   end
