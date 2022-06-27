07d5b463023c2084f9cb6fee364a02c2d4648aca
qinmingyuan <mingyuan0715@foxmail.com>
2022-06-27 14:46:40 +0800

    permit

--- app/controllers/profiled/my/addresses_controller.rb
+++ app/controllers/profiled/my/addresses_controller.rb
@@ -14,7 +14,17 @@ module Profiled
     end
 
     def new
-      @address = current_user.addresses.build
+      r = QqMapHelper.ip '110.53.215.204' || request.remote_ip
+      area_params = r['ad_info']
+      if area_params
+        area = Area.sure_find_full [area_params['province'], area_params['city'], area_params['district'].presence].compact
+      else
+        area = Area.new
+      end
+      @address.area = area
+    end
+
+    def order_new
       r = QqMapHelper.ip '110.53.215.204' || request.remote_ip
       area_params = r['ad_info']
       if area_params
