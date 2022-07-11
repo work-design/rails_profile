b130c22d9876d4a6554fb7bf9e0b85e4b7546f32
qinmingyuan <mingyuan0715@foxmail.com>
2022-07-12 00:14:50 +0800

    account

--- app/models/profiled/ext/account.rb
+++ app/models/profiled/ext/account.rb
@@ -4,8 +4,6 @@ module Profiled
 
     included do
       has_many :profiles, class_name: 'Profiled::Profile', primary_key: :identity, foreign_key: :identity
-      #has_many :proteges, through: :profiles
-      #has_many :maintains, through: :proteges, source: :maintains
     end
 
   end
