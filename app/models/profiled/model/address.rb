ca556f432a2f447f55a0195acfc0684193adffad
qinmingyuan <mingyuan0715@foxmail.com>
2022-08-02 18:28:23 +0800

    add agent userid

--- app/models/profiled/model/address.rb
+++ app/models/profiled/model/address.rb
@@ -15,10 +15,14 @@ module Profiled
       has_taxons :area
       belongs_to :area, class_name: 'Profiled::Area'
       belongs_to :organ, class_name: 'Org::Organ', optional: true
+
       belongs_to :user, class_name: 'Auth::User', optional: true
       belongs_to :member, class_name: 'Org::Member', optional: true
       belongs_to :member_organ, class_name: 'Org::Organ', optional: true
-      belongs_to :agent, class_name: 'Org::Member', optional: true
+
+      belongs_to :agent_user, class_name: 'Auth::User', optional: true
+      belongs_to :agent_member, class_name: 'Org::Member', optional: true
+      belongs_to :agent_organ, class_name: 'Org::Organ', optional: true
 
       has_many :address_uses, inverse_of: :address, dependent: :destroy_async
 
