709b19ad965f9375a11bfd0b41093a3e1bf261dc
qinmingyuan <mingyuan0715@foxmail.com>
2022-10-07 16:21:53 +0800

    gemfile

--- app/models/profiled/ext/organ.rb
+++ app/models/profiled/ext/organ.rb
@@ -3,11 +3,9 @@ module Profiled
     extend ActiveSupport::Concern
 
     included do
-      has_many :address_organs, class_name: 'Profiled::AddressOrgan', inverse_of: :organ, dependent: :delete_all
       has_many :addresses, class_name: 'Profiled::Address'
 
-      has_one :default_address_organ, -> { where(default: true) }, class_name: 'Profiled::AddressOrgan'
-      has_one :default_address, class_name: 'Profiled::Address', through: :default_address_organ, source: :address
+      has_one :default_address, class_name: 'Profiled::Address'
     end
 
   end
