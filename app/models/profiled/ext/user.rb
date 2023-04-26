71df369deb5db4ef3381e0a80a912e9ea5cee4cf
qinmingyuan <mingyuan0715@foxmail.com>
2023-04-26 13:00:39 +0800

    profiled

--- app/models/profiled/ext/user.rb
+++ app/models/profiled/ext/user.rb
@@ -3,7 +3,8 @@ module Profiled
     extend ActiveSupport::Concern
 
     included do
-      has_many :addresses, class_name: 'Profiled::Address', dependent: :destroy_async
+      has_many :profiles, class_name: 'Profiled::Profile'
+      has_many :addresses, class_name: 'Profiled::Address'
       has_many :principal_addresses, class_name: 'Profiled::Address'
     end
 
