257ae11d323e65ee8af7e5a2725d90cc9d983022
qinmingyuan <mingyuan0715@foxmail.com>
2023-11-02 17:23:21 +0800

    gemfiel

--- app/models/profiled/address.rb
+++ app/models/profiled/address.rb
@@ -4,5 +4,6 @@ module Profiled
     include Factory::Model::Address if defined? RailsFactory
     include Ship::Ext::Address if defined? RailsShip
     include Wait::Model::Address if defined? RailsWait
+    include Crm::Ext::Maintainable if defined? RailsCrm
   end
 end
